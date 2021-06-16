2d58383ac72f177cb5a17efab6b617e0dc511542
David Lipshitz <victor.david.lip@gmail.com>
2021-06-16 05:46:52 -0300

    fixed random number function

--- app.js
+++ app.js
@@ -97,5 +97,5 @@ function juegoTerminado(gano, mensaje) {
 
 //genero un numero aleatorio entre los valores asignados previamente
 function numAleatorio(min, max) {
-  return Math.floor(Math.random() * (max - min + 1));
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
